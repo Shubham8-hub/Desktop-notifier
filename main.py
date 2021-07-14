61af0a74ec8e7cfe9c07719a74cd70977d55b55e
Shubham <samalshubham8@gmail.com>
2021-07-14 20:29:18 +0530

    simple notifier

---
+++ main.py
@@ -0,0 +1,8 @@
+from plyer import notification
+
+# if __name__ == "__main__":
+notification.notify(
+    title = "Please Drink Water",
+    message = "It is important for body",
+    timeout = 5
+)
